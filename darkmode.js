f99a2fe1784254d6fc3460fc7f5f9482a9716712
Jordi <hovenjordi@gmail.com>
2018-11-26 13:59:47 +0100

    Recommit TypeIt.js

---
+++ darkmode.js
@@ -0,0 +1,19 @@
+// Dark mode
+$("#switch").on("click", () => {
+    if($("#switch").prop("checked")) {
+      $("body").addClass("dark");
+      $("main").addClass("dark-text");
+      $(".mail-btn").addClass("dark-btn");
+      $(".jordakoes").addClass("dark-portfolio-bg");
+      $(".flickpick").addClass("dark-portfolio-bg");
+      $(".Typewriter__cursor").addClass("dark-cursor");
+    } 
+    else {
+      $("body").removeClass("dark");
+      $("main").removeClass("dark-text");
+      $(".mail-btn").removeClass("dark-btn");
+      $(".jordakoes").removeClass("dark-portfolio-bg");
+      $(".flickpick").removeClass("dark-portfolio-bg");
+      $(".Typewriter__cursor").removeClass("dark-cursor");
+    }
+  })
--- script.js
+++ script.js
@@ -1,35 +1,7 @@
-// TODO: Change id='app' to something descriptive
-// FIXME: Slows down over time..
-var app = document.getElementById('typewriter');
-
-var typewriter = new Typewriter(app, {
+new TypeIt('#typewriter', {
+  strings: ["This is a great string.", "But here is a better one."],
+  speed: 150,
+  breakLines: false,
+  autoStart: false,
   loop: true
 });
-
-typewriter
-  .typeString('Design')
-  .deleteAll()
-  .typeString('Development')
-  .deleteAll()
-  .typeString('Deployment')
-  .start();
-    
-// Dark mode
-$("#switch").on("click", () => {
-  if($("#switch").prop("checked")) {
-    $("body").addClass("dark");
-    $("main").addClass("dark-text");
-    $(".mail-btn").addClass("dark-btn");
-    $(".jordakoes").addClass("dark-portfolio-bg");
-    $(".flickpick").addClass("dark-portfolio-bg");
-    $(".Typewriter__cursor").addClass("dark-cursor");
-  } 
-  else {
-    $("body").removeClass("dark");
-    $("main").removeClass("dark-text");
-    $(".mail-btn").removeClass("dark-btn");
-    $(".jordakoes").removeClass("dark-portfolio-bg");
-    $(".flickpick").removeClass("dark-portfolio-bg");
-    $(".Typewriter__cursor").removeClass("dark-cursor");
-  }
-})
